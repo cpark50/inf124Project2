dc2b0f03f14fad706e0a2e644713f622ea288542
Cindy Park <ccindyy82021@gmail.com>
2022-04-25 16:24:23 -0700

    initial commit

---
+++ src/main/java/com/example/justplants/trial.java
@@ -0,0 +1,54 @@
+package com.example.justplants;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "trial", value = "/trial")
+public class trial extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/" + "pa124", "root", "cindy1234");
+            Statement stmt = con.createStatement();
+            String sql = "SELECT * FROM product";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            PrintWriter writer = resp.getWriter();
+            writer.println("<html> <body>");
+            while(rs.next()){
+                String name = rs.getString("p_name");
+                String image = rs.getString("imagename");
+                Integer price = rs.getInt("p_price");
+                writer.println("<div><a href=\"plants/peace_lily.html\"><img src=\"images/" + image +"\" alt=\"" + name + "\">");
+                writer.println("<p class=\"pname\">" + name + "</p>");
+                writer.println("<p class=\"price\"> $" + price + ".00</p></a></div>");             
+            }
+            writer.println("</body> </html>");
+        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INF124 PA1</title>
+    <link rel="stylesheet" href="styles/mainpage.css">
+</head>
+
+<a href="./trial">Click Me!</a>
