d83a33070719e73bc8df41db7d3378a330d40aec
Rongbing Lai <rongbil1@uci.edu>
2022-04-30 22:21:35 -0700

    add recentorder file and use include

--- src/main/java/com/example/justplants/recentorder.java
+++ src/main/java/com/example/justplants/recentorder.java
@@ -8,7 +8,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,36 +15,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-
-//when to close the db? 
-//add information to and query table
-@WebServlet(name = "home", value = "/home") //@WebServlet(name="JDBC Demo", urlPatterns="/link")
-public class mainpage extends HttpServlet {
+@WebServlet(name = "recentorder", value = "/recentorder")
+public class recentorder extends HttpServlet{
 
     @Override
     public void init() throws ServletException {
         super.init();
         
     }
-    
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try{ //slow. connection. 
+        try{
             Class.forName("com.mysql.jdbc.Driver"); //load library
             Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/" + "JustPlantsProducts", "root", "aliceqiu367");
             Statement stmt = con.createStatement();
-            String sql = "SELECT * FROM products";
+            String sql = "SELECT * FROM order_info";
             ResultSet rs = stmt.executeQuery(sql);
 
-            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/recentorder");
-            requestDispatcher.include(req, resp);
-            
+            PrintWriter writer = resp.getWriter();
+
             int count = 1;
 
-            PrintWriter writer = resp.getWriter();
-            writer.println("<html> <head> <link rel=\"stylesheet\" href=\"styles/mainpage.css\"> <title>Just Plants</title> </head>");
-            writer.println("<body> <div class=\"title\"><h1><a href=\"home\">JustPlants</a></h1></div>");
-            writer.println("<div class=\"nav_bar\"><ul><li><a class=\"active\" href=\"home\">Home</a></li><li><a href=\"aboutcompany.html\">About Company</a></li><li><a href=\"orderInfo\">Make Order</a></li></ul></div>");
+            writer.println("<html><body>");
             while(rs.next()){
 
                 if (count == 5)
@@ -54,8 +46,7 @@ public class mainpage extends HttpServlet {
                 String name = rs.getString("p_name");
                 String image = rs.getString("imagename");
                 Integer price = rs.getInt("p_price");
-                //boolean: getBlob
-                //send to product page with id number for the database
+
                 writer.println("<div class=\"col-" + count + "\"><a href=\"./product\"><img src=\"images/" + image +"\" alt=\"" + name + "\">");
                 writer.println("<p class=\"pname\">" + name + "</p>");
                 writer.println("<p class=\"price\"> $" + price + ".00</p></a></div>");
@@ -69,6 +60,5 @@ public class mainpage extends HttpServlet {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
     }
 }
