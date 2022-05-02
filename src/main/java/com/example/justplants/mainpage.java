0ea482a4d6851fc0bfbffe11b4d9708d80dd17c1
Cindy Park <ccindyy82021@gmail.com>
2022-05-01 18:06:34 -0700

    product pages and directories updated

--- src/main/java/com/example/justplants/mainpage.java
+++ src/main/java/com/example/justplants/mainpage.java
@@ -10,17 +10,19 @@ import java.sql.Statement;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 
 //when to close the db? 
 //add information to and query table
-@WebServlet(name = "home", value = "/home") //@WebServlet(name="JDBC Demo", urlPatterns="/link")
-public class mainpage extends HttpServlet {
+@WebServlet(name = "home", urlPatterns = "") //@WebServlet(name="JDBC Demo", urlPatterns="/link")
+public class Mainpage extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
@@ -30,9 +32,10 @@ public class mainpage extends HttpServlet {
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(true);
         try{ //slow. connection. 
             Class.forName("com.mysql.jdbc.Driver"); //load library
-            Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/" + "JustPlantsProducts", "root", "aliceqiu367");
+            Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/" + "pa124", "root", "cindy1234");
             Statement stmt = con.createStatement();
             String sql = "SELECT * FROM products";
             ResultSet rs = stmt.executeQuery(sql);
@@ -44,8 +47,8 @@ public class mainpage extends HttpServlet {
 
             PrintWriter writer = resp.getWriter();
             writer.println("<html> <head> <link rel=\"stylesheet\" href=\"styles/mainpage.css\"> <title>Just Plants</title> </head>");
-            writer.println("<body> <div class=\"title\"><h1><a href=\"home\">JustPlants</a></h1></div>");
-            writer.println("<div class=\"nav_bar\"><ul><li><a class=\"active\" href=\"home\">Home</a></li><li><a href=\"aboutcompany.html\">About Company</a></li><li><a href=\"orderInfo\">Make Order</a></li></ul></div>");
+            writer.println("<body> <div class=\"title\"><h1><a href=\"\">JustPlants</a></h1></div>");
+            writer.println("<div class=\"nav_bar\"><ul><li><a class=\"active\" href=\"\">Home</a></li><li><a href=\"aboutcompany.html\">About Company</a></li><li><a href=\"orderInfo\">Make Order</a></li></ul></div>");
             while(rs.next()){
 
                 if (count == 5)
@@ -54,13 +57,15 @@ public class mainpage extends HttpServlet {
                 String name = rs.getString("p_name");
                 String image = rs.getString("imagename");
                 Integer price = rs.getInt("p_price");
-                //boolean: getBlob
-                //send to product page with id number for the database
-                writer.println("<div class=\"col-" + count + "\"><a href=\"./product\"><img src=\"images/" + image +"\" alt=\"" + name + "\">");
+                Integer p_id = rs.getInt("id");
+                
+                //send to product page with p_id number for the database
+                writer.println("<div class=\"col-" + count + "\" id=\""+ p_id +"\"><a href=\"./product/"+p_id+"\"><img src=\"images/" + image +"\" alt=\"" + name + "\">");
                 writer.println("<p class=\"pname\">" + name + "</p>");
                 writer.println("<p class=\"price\"> $" + price + ".00</p></a></div>");
                 count++;             
             }
+            writer.println("");
             writer.println("</body> </html>");
         }
         catch(ClassNotFoundException e){
--- src/main/java/com/example/justplants/product.java
+++ src/main/java/com/example/justplants/product.java
@@ -15,8 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(name = "product", value = "/product")
-public class product extends HttpServlet {
+@WebServlet(name = "product")
+public class Product extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
@@ -25,26 +25,67 @@ public class product extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = "";
+        String image = "";
+        Integer price = 0;
+        String size = "";
+        String othername = "";
+        String dscrptn = "";
+        String water = "";
+        String light = "";
+        String friend = "best kept away from pets and children";        
+        
         try{ //slow. connection. 
+            String str_url = req.getRequestURL().toString();
+            String plant_id = str_url.substring(str_url.lastIndexOf('/')+1);
             Class.forName("com.mysql.jdbc.Driver"); //load library
-            Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/" + "JustPlantsProducts", "root", "aliceqiu367");
+            Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/" + "pa124", "root", "cindy1234");
             Statement stmt = con.createStatement();
-            String sql = "SELECT * FROM products";
+            String sql = "SELECT * FROM products WHERE id =" + plant_id;
             ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                name = rs.getString("p_name");
+                image = rs.getString("imagename");
+                price = rs.getInt("p_price");
+                othername = rs.getString("p_othername");
+                dscrptn = rs.getString("p_desc")+rs.getString("p_desc2");
+                size = rs.getString("p_size");
+                water = rs.getString("p_water");
+                light = rs.getString("p_light");
+                friend = "best kept away from pets and children";
+                if (rs.getBoolean("p_pet")){
+                    friend = "pet and children friendly";
+                }
+            }
 
             PrintWriter writer = resp.getWriter();
-            writer.println("<html> <body>");
-            while(rs.next()){
-                String name = rs.getString("p_name");
-                String image = rs.getString("imagename");
-                Integer price = rs.getInt("p_price");
+            writer.println("<html> <head> <link rel=\"stylesheet\" href=\"../styles/productpage.css\"> <title>Just Plants</title> </head>");
+            writer.println("<body> <div class=\"title\"><h1><a href=\"../\">JustPlants</a></h1></div>");
+            writer.println("<div class=\"nav_bar\"><ul><li><a class=\"active\" href=\"../\">Home</a></li><li><a href=\"../aboutcompany.html\">About Company</a></li><li><a href=\"../orderInfo\">Make Order</a></li></ul></div>");
+            writer.println("<div class=\"item-title\">");
+            writer.println("<span>Plants</span><h1>"+ name + "</h1></div>");
+            writer.println("<main class = \"container\">");
+            writer.println("<div class=\"left-column\"><img data-image=\"red\" class=\"active\" src=\"../images/"+image+"\"></div>");
+            writer.println("<div class=\"right-column\">");
+
+            writer.println("<div class=\"product-description\">");
+            writer.println("<p> <i>"+ othername + "</i></p>");
+            writer.println("<p>"+dscrptn+"</p>");
+            writer.println("<i>size (height × width × diameter): <br>");
+            writer.println(size+"</i><br><br></i>");
+            writer.println("<i>How to take care:</i><br>");
+            writer.println("light: "+light+"<br>");
+            writer.println("water: "+water+"<br>");
+            writer.println("friendliness: "+friend+"<br></p></div>");
+            writer.println("</div></div>");
+
                 //boolean: getBlob
                 //send to product page with id number for the database
-                writer.println("<div><a href=\"./product\"><img src=\"images/" + image +"\" alt=\"" + name + "\">");
-                writer.println("<p class=\"pname\">" + name + "</p>");
-                writer.println("<p class=\"price\"> $" + price + ".00</p></a></div>");             
-            }
-            writer.println("</body> </html>");
+        //     writer.println("<div><a href=\"./product\"><img src=\"../images/" + image +"\" alt=\"" + name + "\">");
+        //     writer.println("<p class=\"pname\">" + name + "</p>");
+        //     writer.println("<p class=\"price\"> $" + price + ".00</p></a></div>");             
+        
+        //     writer.println("</body> </html>");
         }
         catch(ClassNotFoundException e){
             e.printStackTrace();
